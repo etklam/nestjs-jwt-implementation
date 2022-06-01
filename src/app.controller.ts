7bbe3b809d61e1741cbbb0091c2b9756868be7cf
etklam <mail@kwunlam.com>
2022-06-01 19:40:32 +0800

    add jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,37 +1,50 @@
 import {
   Controller,
   Get,
+  Request,
   Header,
   Post,
   UploadedFile,
   UploadedFiles,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+  ) {}
 
-  @Get()
-  @Header('Content-Type', 'text/html')
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  // @Get()
+  // @Header('Content-Type', 'text/html')
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+  //
+  // @Post('/single')
+  // @UseInterceptors(FileInterceptor('file'))
+  // uploadSingleFile(@UploadedFile() file: Express.Multer.File) {
+  //   return file;
+  // }
+  //
+  // @Post('/uploads')
+  // @UseInterceptors(FilesInterceptor('files'))
+  // uploadMultipleFiles(@UploadedFiles() files: Express.Multer.File[]) {
+  //   return files.map(({ fieldname, originalname }) => ({
+  //     fieldname,
+  //     originalname,
+  //   }));
+  // }
 
-  @Post('/single')
-  @UseInterceptors(FileInterceptor('file'))
-  uploadSingleFile(@UploadedFile() file: Express.Multer.File) {
-    return file;
-  }
-
-  @Post('/uploads')
-  @UseInterceptors(FilesInterceptor('files'))
-  uploadMultipleFiles(@UploadedFiles() files: Express.Multer.File[]) {
-    return files.map(({ fieldname, originalname }) => ({
-      fieldname,
-      originalname,
-    }));
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 import 'dotenv/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(`mongodb://localhost:27017/mydb`)],
+  imports: [MongooseModule.forRoot(`mongodb://localhost:27017/mydb`), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
