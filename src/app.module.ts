6f7d1cd327f2f2d707f2abdd94a61c429e3031a1
etklam <mail@kwunlam.com>
2022-06-01 19:10:12 +0800

    add mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-// const MONGO = {
-//   username: 'klam',
-//   password: encodeURIComponent('klamdevenv'),
-//   getUrl: function () {
-//     return `mongodb://localhost:${this.username}:${this.password}}@shorter`;
-//   },
-// };
+import 'dotenv/config';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(`mongodb://localhost:27017/mydb`)],
   controllers: [AppController],
   providers: [AppService],
 })
