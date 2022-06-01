dfd224aa6b1837334bba0fac3e4e385fd8b0216a
etklam <mail@kwunlam.com>
2022-06-01 19:48:18 +0800

    add jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,6 +13,7 @@ import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -21,30 +22,36 @@ export class AppController {
     private authService: AuthService,
   ) {}
 
-  // @Get()
-  // @Header('Content-Type', 'text/html')
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
-  //
-  // @Post('/single')
-  // @UseInterceptors(FileInterceptor('file'))
-  // uploadSingleFile(@UploadedFile() file: Express.Multer.File) {
-  //   return file;
-  // }
-  //
-  // @Post('/uploads')
-  // @UseInterceptors(FilesInterceptor('files'))
-  // uploadMultipleFiles(@UploadedFiles() files: Express.Multer.File[]) {
-  //   return files.map(({ fieldname, originalname }) => ({
-  //     fieldname,
-  //     originalname,
-  //   }));
-  // }
+  @Get()
+  @Header('Content-Type', 'text/html')
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('/single')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadSingleFile(@UploadedFile() file: Express.Multer.File) {
+    return file;
+  }
+
+  @Post('/uploads')
+  @UseInterceptors(FilesInterceptor('files'))
+  uploadMultipleFiles(@UploadedFiles() files: Express.Multer.File[]) {
+    return files.map(({ fieldname, originalname }) => ({
+      fieldname,
+      originalname,
+    }));
+  }
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
