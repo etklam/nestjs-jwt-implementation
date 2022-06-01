4da9ce9220d7937992484a89605e99834fc3e4d1
etklam <mail@kwunlam.com>
2022-06-01 17:41:20 +0800

    uploader

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +15,23 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Header('Content-Type', 'text/html')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/single')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadSingleFile(@UploadedFile() file: Express.Multer.File) {
+    return file;
+  }
+
+  @Post('/uploads')
+  @UseInterceptors(FilesInterceptor('files'))
+  uploadMultipleFiles(@UploadedFiles() files: Express.Multer.File[]) {
+    return files.map(({ fieldname, originalname }) => ({
+      fieldname,
+      originalname,
+    }));
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+// const MONGO = {
+//   username: 'klam',
+//   password: encodeURIComponent('klamdevenv'),
+//   getUrl: function () {
+//     return `mongodb://localhost:${this.username}:${this.password}}@shorter`;
+//   },
+// };
 
 @Module({
   imports: [],
